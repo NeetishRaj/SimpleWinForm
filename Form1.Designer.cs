d3425d7f76849f26e2715610dd5edced8af2c0f9
dev <dev@smarttask.io>
2021-06-03 02:41:14 +0530

    Complete WindowsFormApplication added

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace SimpleWinForm
+{
+    partial class Matrix
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Matrix));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Morpheus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Size = new System.Drawing.Size(561, 311);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Location = new System.Drawing.Point(107, 337);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Hi Neetish!";
+            this.textBox1.Size = new System.Drawing.Size(467, 60);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Morpheus
+            // 
+            this.Morpheus.AutoSize = true;
+            this.Morpheus.Location = new System.Drawing.Point(8, 350);
+            this.Morpheus.Name = "Morpheus";
+            this.Morpheus.Size = new System.Drawing.Size(95, 23);
+            this.Morpheus.TabIndex = 2;
+            this.Morpheus.Text = "Morpheus:";
+            this.Morpheus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Matrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 413);
+            this.Controls.Add(this.Morpheus);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.MaximumSize = new System.Drawing.Size(601, 470);
+            this.MinimumSize = new System.Drawing.Size(200, 150);
+            this.Name = "Matrix";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Matrix";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Morpheus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinForm
+{
+    public partial class Matrix : Form
+    {
+        private string[] Messages = { 
+            "You're better than this!",
+            "You are the best!",
+            "Remember, You are among greatness!",
+            "You chose this and you choose wisely!",
+            "You never take the easy route!",
+            "I thought weakness disgusts you!",
+            "Just do it! Don't think much about it.",
+            "Listen!, I know you are smart but where's dedication?",
+            "Never forget your long term goals!",
+            "Healthy mind resides in a healthy body!",
+            "What good is money, if you are not fit to enjoy it!",
+            "Why ignoring me?",
+            "So.. How was your morning run today?",
+            "Again planning to eat unhealthy?",
+            "Don't forget to have some fun!",
+            "Respect and trust builds foundation!",
+            "Don't make me come out of screen to beat your ass!",
+            "Tum kya lekar aaye the aur kya lekar jaaoge?",
+            "Body will become dust but karma will stay many generations",
+            "You can tame any element of nature",
+            "Remember that day when you helped that kid get his first job",
+            "Your Dad is proud of you!",
+            "You suck!",
+            "Today is not your day my friend. Just relax!",
+            "Let your work speak for you!",
+            "Short term goals helps build discipline for long term goals",
+            "Just be yourself!",
+            "You are an alpha! Act like one!",
+            "How are you doing, Neetish?",
+            "Remember me?",
+            "You just talk big!",
+            "Just chill!",
+            "You strength gives other hope!"
+        };
+        private Timer tmr;
+        private int AUTOMATIC_CLOSINGTIME = 1800;
+        private int getRandomInt(int low = 0, int high = 10)
+        {
+            Random rand = new Random();
+            int randIntValue = rand.Next(low, high);
+
+            return randIntValue;
+        }
+        private void SetupAutoCloseTimer() {
+            tmr = new Timer();
+            tmr.Tick += delegate {
+                this.Close();
+            };
+            tmr.Interval = (int)TimeSpan.FromSeconds(AUTOMATIC_CLOSINGTIME).TotalMilliseconds;
+            tmr.Start();
+        }
+        public Matrix()
+        {
+            InitializeComponent();
+            textBox1.Text = Messages[getRandomInt(0, Messages.Length)];
+            textBox1.Enabled = false;
+
+            SetupAutoCloseTimer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
